a818799ceea0253b283b16269cce33f4d73f895d
jiangjingming <baili@maihaoche.com>
2017-08-06 22:48:12 +0800

    命名规范

--- springbootstudyweb/src/test/java/com/baili/springboot/web/test/ApplicationTests.java
+++ springbootstudyweb/src/test/java/com/baili/springboot/web/test/ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.baili.springboot.web.test;
 
 import com.baili.springboot.study.Application;
-import org.apache.commons.collections.map.HashedMap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
