32dbc0b550c5c3291d4af388d610db9cbe37186b
jiangjingming <baili@maihaoche.com>
2017-08-06 17:01:43 +0800

    发送邮件功能集成

---
+++ springbootstudyweb/src/test/java/com/baili/springboot/web/test/ApplicationTests.java
@@ -0,0 +1,74 @@
+package com.baili.springboot.web.test;
+
+import com.baili.springboot.study.Application;
+import org.apache.commons.collections.map.HashedMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+/**
+ * Created by jiangjingming on 2017/8/6.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTests {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Test
+    public void sendSimpleMail() throws Exception {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("jiangjingmingjava@163.com");
+        message.setTo("jiangjingmingjava@163.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容3");
+
+        mailSender.send(message);
+    }
+
+    @Test
+    public void sendAttachmentsMail() throws Exception {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setFrom("jiangjingmingjava@163.com");
+        helper.setTo("jiangjingmingjava@163.com");
+        helper.setSubject("主题：有附件");
+        helper.setText("有附件的邮件");
+
+        FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+        helper.addAttachment("附件-1.jpg", file);
+        helper.addAttachment("附件-2.jpg", file);
+
+        mailSender.send(mimeMessage);
+    }
+
+    @Test
+    public void sendInlineMail() throws Exception {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setFrom("jiangjingmingjava@163.com");
+        helper.setTo("jiangjingmingjava@163.com");
+        helper.setSubject("主题：嵌入静态资源");
+        helper.setText("<html><body><img src=\"cid:weixin\" ></body></html>", true);
+
+        FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+        helper.addInline("weixin", file);
+
+        mailSender.send(mimeMessage);
+    }
+
+}
